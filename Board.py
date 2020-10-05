a5863584346a642e2585392722611c2d51c2e9d3
Oliver Thode <omthode@wpi.edu>
2020-10-04 21:50:47 -0700

    Basic IO methods and formatting
    might need to rewrite as we learn more

--- Board.py
+++ Board.py
@@ -1,21 +1,19 @@
 class Board:
-
-    #-1 empty, 0 white piece, 1 black piece
-    board : list
-    placedPieces : list
+    # -1 empty, 0 white piece, 1 black piece
+    board: list
+    placedPieces: list
 
     def __init__(self):
-        self.board = [ [-1]*15 for i in range(15)]
-            
+        self.board = [[-1] * 15 for i in range(15)]
 
-    def setPiece(self, x : int, y : int, piece : int):
-            self.board[x][y] = int
+    def setPiece(self, x: int, y: int, piece: int):
+        self.board[x][y] = int
+
 
 class Location:
+    x: int
+    y: int
 
-    x : int
-    y : int
-
-    def __init__(self, x : int, y : int):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
--- IO.py
+++ IO.py
@@ -1,1 +1,37 @@
-#
+import sys
+from os import path
+
+move_file_name = "move_file"
+end_file_name = "end_game"
+
+
+class IO:
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    def ready(self):
+        return path.exists(self.name + ".go")
+
+
+def read_move():
+    min_chars_in_line = 3
+    with open(move_file_name, 'r') as move_file:
+        for line in move_file.readlines():
+            if len(line) > min_chars_in_line:
+                line = line.strip()
+                name, col, row = line.split(' ')
+                print("Move: " + name + " at (" + row + "," + col + ")")
+
+
+def write_move(move):
+    with open(move_file_name, 'w') as move_file:
+        move_txt = str(move)
+        move_file.write(move_txt)
+        move_file.write("\n")
+        move_file.flush()
+
+
+def game_over():
+    return path.exists(end_file_name)
